bf05e82425623e1b3897232ad1e058da1fe0a1c9
asmodeus55 <74595761+asmodeus55@users.noreply.github.com>
2020-11-17 18:07:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'ASMODEUS BOT☄'; 
+const instagram = 'https://instagram.com/rrnabila_ayu_w.id'; 
+const telegram = 'https://t.me/★𝖉𝖜𝖎𝖕𝖊𝖊★'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/c/hipster.id';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
